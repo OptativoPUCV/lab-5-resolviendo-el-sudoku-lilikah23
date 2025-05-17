f0ad48d59c9ce3847d06217d44819a094413250a
Your Name <you@example.com>
2025-05-17 15:06:23 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -122,8 +122,7 @@ int is_final(Node* n){
             if(n->sudo[i][j] == 0) return 0; // Hay una celda vacía
         }
     }
-    // Si no hay celdas vacías, verificamos si es válido
-    return is_valid(n);
+    return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
